b53e4b5435b80f4244f5383bd1eaf197414c6a5b
mid19 <mid19@DESKTOP-3LV7E2Q>
2019-01-19 21:26:20 -0800

    updated

--- ConsoleApp1/FibonacciNumber.cs
+++ ConsoleApp1/FibonacciNumber.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         public int Fib(int N)
         {
+            // Check boundary
             if (N == 0)
                 return 0;
 
---
+++ ConsoleApp1/LowerCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class LowerCase
+{
+    public string ToLowerCase(string str)
+    {
+
+        if (string.IsNullOrEmpty(str))
+        {
+            return "";
+        }
+
+        int len = str.Length;
+        char[] data = str.ToArray();
+        for (int i = 0; i < len; i++)
+        {
+            char c = data[i];
+            if (c >= 'A' && c <= 'Z')
+            {
+                //do something to lower case
+                int distance = (c + 'a' - 'A');
+                data[i] = (char)distance;
+            }
+        }
+
+        string str1 = new string(data);
+        return str1;
+    }
+}
